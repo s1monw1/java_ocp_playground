89c5826fe41334f3b0ddaf7848cfc64a2bc0d448
Simon <simon.wirtzq@gmail.com>
2017-05-29 21:50:50 +0200

    array variance trial

---
+++ src/main/java/de/swirtz/playground/variance/ArrayVariance.java
@@ -0,0 +1,13 @@
+package de.swirtz.playground.variance;
+
+/**
+ * Created by simonw on 28.05.17.
+ */
+public class ArrayVariance {
+
+    public static void main(String[] args) {
+        Object [] arr = new String [] {"hello", "world"};
+        arr[1] = new Object();
+
+    }
+}
